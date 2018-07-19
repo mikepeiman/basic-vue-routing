63fe1fd3d6cdd793742b223beaf479610c555c28
Mike Peiman <mikepeiman@gmail.com>
2018-07-18 17:22:08 -0700

    set up basic three-link navbar with router-link. Compared hardcoded 'to' with v-bind :to

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>About</h1>
+</template>
+
+<script>
+export default {
+  name:'About',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About-normal</router-link></li>
+      <li><router-link :to="{ name: 'About' }">About-bind</router-link></li>
+      <li><router-link to="/blog">Blog</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+nav {
+  width: 90vw;
+  display: flex;
+  justify-content: center;
+}
+nav ul {
+  display: flex;
+  width: 80%;
+  list-style-type: none;
+  flex-direction: row;
+  justify-content: space-between;
+  /* align-content: space-around; */
+  /* align-items: center; */
+}
+nav ul li {
+  justify-content: space-around;
+
+}
+</style>
+
