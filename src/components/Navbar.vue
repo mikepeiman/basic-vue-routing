c51f4f6ff542ef9bb704e7eaf7a270d8a3909f5b
Mike Peiman <mikepeiman@gmail.com>
2018-10-07 11:45:34 -0700

    catching up commit from 3 months ago

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,11 +5,18 @@
       <li><router-link to="/about">About-normal</router-link></li>
       <li><router-link :to="{ name: 'About' }">About-bind</router-link></li>
       <li><router-link to="/blog">Blog</router-link></li>
+      
       <ul>
-        <li><h2>User Profiles</h2></li>
+        <h2>User Profiles</h2>
         <li v-for="(id, index) in userIds" :key="index">
           <router-link :to="{ name: 'ViewProfile', params: { user_id: id } }">{{ id }}</router-link>
         </li>
+      </ul>
+            <ul>
+        <h2>Nav Controls</h2>
+        <li><button @click="goHome">Redirect to Home</button></li>
+        <li><button @click="goBack">Back</button></li>
+        <li><button @click="goForward">Forward</button></li>
       </ul>
     </ul>
   </nav>
@@ -22,18 +29,31 @@ export default {
     return {
       userIds: ['1','2','3','4']
     };
+  },
+  methods: {
+    goHome() {
+      this.$router.push({ name: 'Home' })
+    },
+    goBack() {
+      this.$router.go(-1)
+    },
+    goForward() {
+      this.$router.go(1)
+    }
   }
 };
 </script>
 
 <style>
+$color-main: #aa22ff;
 nav {
   width: 90vw;
   display: flex;
   justify-content: center;
+  
 }
 nav ul {
-  display: flex;
+  /* display: flex; */
   /* width: 80%;   */
   list-style-type: none;
   flex-direction: row;
@@ -43,11 +63,28 @@ nav ul {
 }
 nav ul li {
   justify-content: space-around;
+  display: inline-block;
   margin: 0 5px;
 }
 nav ul ul {
-  display: flex;
+  /* display: flex; */
   flex-direction: column;
 }
+a {
+  color: #a45bac;
+  text-decoration: none;
+}
+a:hover {
+  color: #145bff;
+}
+a.router-link-exact-active {
+  /* text-decoration: underline; */
+  background: #333;
+  color: #eee;
+  padding: 5px;
+}
+/* a:visited {
+  color: #145b7c;
+} */
 </style>
 
